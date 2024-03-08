de45b4644b88ace3f8d88942bf92a6b36463c0dc
Rainlost331 <artem2004d04@mail.ru>
2024-03-08 16:59:40 +0300

    Копирование файлов из лаб. 6 в лаб. 7

---
+++ app/src/main/java/com/topic2/android/notes/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.topic2.android.notes
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.compose.setContent
+import com.topic2.android.notes.theme.NotesTheme
+import com.topic2.android.notes.viewmodel.MainViewModel
+import com.topic2.android.notes.viewmodel.MainViewModelFactory
+import ui.components.screens.NotesScreen
+
+/**
+ * Main activity приложения.
+ */
+class MainActivity : AppCompatActivity() {
+
+  private val viewModel: MainViewModel by viewModels(factoryProducer = {
+    MainViewModelFactory(
+      this,
+      (application as NotesApplication).dependencyInjector.repository
+    )
+  })
+
+  @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    setContent {
+      NotesTheme {
+        NotesScreen(viewModel = viewModel)
+      }
+    }
+  }
+}
